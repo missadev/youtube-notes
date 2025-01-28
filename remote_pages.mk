f061a1d187588bfa58dbb291e7f7a1063632535c
missadev <missaelcastillo@hotmail.es>
2025-01-28 17:15:29 -0600

    Add remote job recommendation pages

---
+++ remote_pages.mk
@@ -0,0 +1,47 @@
+# All Pages
+
+| Place | Page Name | Number of Recommendations | Who's Recommending Them |
+| --- | --- | --- | --- |
+| 1 | LinkedIn | 5 | Juanca Tec, Midudev, Dani Conde, Oridigital, Lucía |
+| 2 | [https://flexjobs.com](https://flexjobs.com/) | 4 | Midudev, Oridigital, Lucía, Emprende con Raúl |
+| 3 | [https://arc.dev](https://arc.dev/) | 2 | Juanca Tec, Midudev |
+| 4 | [https://indeed.com](https://indeed.com/) | 3 | H de Leon, Programador X, Dani Conde |
+| 5 | [https://getonbrd.com](https://getonbrd.com/) | 2 | Midudev, Emprende con Raúl |
+1. [https://arc.dev](https://arc.dev/)
+2. [https://belaysolutions.com](https://belaysolutions.com/)
+3. [https://career.proxify.io](https://career.proxify.io/)
+4. [https://dealls.com](https://dealls.com/)
+5. [https://flexjobs.com](https://flexjobs.com/)
+6. [https://fiverr.com](https://fiverr.com/)
+7. [https://freelancer.com](https://freelancer.com/)
+8. [https://getonbrd.com](https://getonbrd.com/)
+9. [https://glints.com](https://glints.com/)
+10. [https://google.com](https://google.com/)
+11. [https://himalayas.app](https://himalayas.app/)
+12. [https://hireline.io](https://hireline.io/)
+13. [https://hubstafftalent.net](https://hubstafftalent.net/)
+14. [https://indeed.com](https://indeed.com/)
+15. [https://infojobs.net](https://infojobs.net/)
+16. [https://jobleads.com](https://jobleads.com/)
+17. [https://jobs.concentrix.com](https://jobs.concentrix.com/)
+18. [https://kalibrr.com](https://kalibrr.com/)
+19. [https://mx.computrabajo.com](https://mx.computrabajo.com/)
+20. [https://occ.com.mx](https://occ.com.mx/)
+21. [https://prosmarketplace.com](https://prosmarketplace.com/)
+22. [https://remote.com](https://remote.com/)
+23. [https://remote.io](https://remote.io/)
+24. [https://remote.work](https://remote.work/)
+25. [https://remoteok.com](https://remoteok.com/)
+26. [https://remotive.com](https://remotive.com/)
+27. [https://remotolist.com](https://remotolist.com/)
+28. [https://snaphunt.com](https://snaphunt.com/)
+29. [https://techinasia.com](https://techinasia.com/)
+30. [https://upwork.com](https://upwork.com/)
+31. [https://vanhack.com](https://vanhack.com/)
+32. [https://virtualvocations.com](https://virtualvocations.com/)
+33. [https://wellfound.com](https://wellfound.com/)
+34. [https://weremoto.com](https://weremoto.com/)
+35. [https://weworkremotely.com](https://weworkremotely.com/)
+36. [https://workana.com](https://workana.com/)
+37. [https://workingnomads.com](https://workingnomads.com/)
+38. [https://www.crossover.com](https://www.crossover.com/)
